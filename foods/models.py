f08c2618d48ba0d68defeabfba94fe24ed27e1cf
bbang2 <power3869@gmail.com>
2025-06-15 16:41:01 +0900

    feat/build Migration file

--- foods/models.py
+++ foods/models.py
@@ -5,7 +5,7 @@ class Menu(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.IntegerField()
-    img_url = models.CharField(max_length=255))
+    img_url = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
